679ff64d19d4f20d92f72370927b377b05f2b901
gnate320 <gnate@rams.colostate.edu>
2013-10-08 23:06:24 -0600

    idk why ss won't wait....

--- awget.cc
+++ awget.cc
@@ -1,3 +1,21 @@
-int main(int argc, const char *argv[]) {
+#include "anonget.h"
 
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+	char myIP[INET6_ADDRSTRLEN];
+	int toSS;
+
+	toSS = prepSocket("cabbage.cs.colosate.edu", "4649", myIP, sizeof(myIP));
+	
+	send(toSS, myIP, strlen(myIP), 0);
+
+	char reply[SSLIST_SIZE];
+	int numbytes;
+	if ((numbytes = recv(toSS, reply, SSLIST_SIZE-1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+
+	close(toSS);
 }
