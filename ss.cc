5579b66a6c045d65da968ab4b3b4a8556149b84d
gnate320 <gnate@rams.colostate.edu>
2013-10-08 23:38:11 -0600

    looping?

--- ss.cc
+++ ss.cc
@@ -61,57 +61,56 @@ int main(int argc, const char *argv[]) {
 		return 1;	
 	}
 	
-	//TODO LOOP and Fork!!
-	printf("Stepping Stone waiting for requests...");
-
-	struct sockaddr_storage incReqAddr;		//address info for incoming request
-	socklen_t incReqSockSize = sizeof(incReqAddr);
-	int incRequestSock = accept(conSock, (struct sockaddr *) &incReqAddr, &incReqSockSize);
-	char incReqIP[INET6_ADDRSTRLEN];
-
-	if (incRequestSock == -1)
+	while (1) 
 	{
-		printf("Error accepting request...");
-		close(conSock);
+		printf("Stepping Stone waiting for requests...");
+
+		struct sockaddr_storage incReqAddr;		//address info for incoming request
+		socklen_t incReqSockSize = sizeof(incReqAddr);
+		int incRequestSock = accept(conSock, (struct sockaddr *) &incReqAddr, &incReqSockSize);
+		char incReqIP[INET6_ADDRSTRLEN];
+
+		if (incRequestSock == -1)
+		{
+			printf("Error accepting request...");
+			close(conSock);
+			close(incRequestSock);
+			//TODO:  Don't exit program, just jump back to top of loop
+			return 1;
+		}
+			
+		//translate the addr struct to an string with IP
+		inet_ntop(incReqAddr.ss_family,
+			getIP((struct sockaddr *)&incReqAddr),
+			incReqIP, sizeof(incReqIP));
+		printf("Got a request from %s", incReqIP);
+		//TODO FORK HERE! 
+		//TODO close conSock for child
+		//TODO recv() stepping stone list + request.
+		//TODO remove THIS Stepping stone IP form the list
+			
+		//TODO if !lastSS :
+			//TODO look up next SS
+			//TODO Connect to next SS
+			//TODO send SS list with request
+			//TODO recv() file "package" as "result"
+		//TODO else lastSS
+			//TODO system.wget(request)
+			//TODO "package" as "result"
+			
+		//TODO send(result) to incRequestSock
+		
+		char sslist[SSLIST_SIZE];
+		memset(sslist, '\0', SSLIST_SIZE);
+		strcat(sslist, "hello, from ");
+		strcat(sslist, myIPstr); 
+		if (send(incRequestSock, sslist, strlen(sslist), 0) == -1) 
+		{
+			printf("Error sending to %s", incReqIP);
+		}		
 		close(incRequestSock);
-		//TODO:  Don't exit program, just jump back to top of loop
-		return 1;
 	}
-	
-	//translate the addr struct to an string with IP
-	inet_ntop(incReqAddr.ss_family,
-		getIP((struct sockaddr *)&incReqAddr),
-		incReqIP, sizeof(incReqIP));
-	printf("Got a request from %s", incReqIP);
 
-	//TODO FORK HERE! 
-	//TODO close conSock for child
-	//TODO recv() stepping stone list + request.
-	//TODO remove THIS Stepping stone IP form the list
-	
-	//TODO if !lastSS :
-		//TODO look up next SS
-		//TODO Connect to next SS
-		//TODO send SS list with request
-		//TODO recv() file "package" as "result"
-	//TODO else lastSS
-		//TODO system.wget(request)
-		//TODO "package" as "result"
-	
-	//TODO send(result) to incRequestSock
-	
-	char sslist[SSLIST_SIZE];
-	memset(sslist, '\0', SSLIST_SIZE);
-	strcat(sslist, "hello, from ");
-	strcat(sslist, myIPstr); 
-	if (send(incRequestSock, sslist, strlen(sslist), 0) == -1) 
-	{
-		printf("Error sending to %s", incReqIP);
-	}	
-	
-	
-	close(incRequestSock);
-	
 	close(conSock);
 	return 0;
 }
