6fb1f2020cdaa1f73f4b42c05267cdfa7c64da39
gnate320 <gnate@rams.colostate.edu>
2013-10-14 00:17:27 -0600

    please

--- anonget.h
+++ anonget.h
@@ -754,7 +754,7 @@ void *handleRequest(void *c)
 		//TODO find the result!!
 		char fname[MAX_URL];
 		memset(fname, '\0', MAX_URL);
-		char *fs = strrchr(request, 242);
+		char* fs = strrchr(request, 242);
 		if (fs == NULL)
 			strcpy(fname, "index.html");	
 		else 
