cb131d9fba891070ffa70ad506f18ce8419a4571
gnate320 <gnate@rams.colostate.edu>
2013-10-14 00:49:46 -0600

    oh now had better work

--- anonget.h
+++ anonget.h
@@ -687,7 +687,7 @@ void *handleRequest(void *c)
 		memset(iCallIt, '\0', MAX_URL);
 		char masher[MAX_URL];
 		memset(masher, '\0', MAX_URL);		
-		sprintf(masher, "%u", getRandom(PORT_LEN, HN_SIZE));
+		sprintf(masher, "%u", getRandom(PORT_LEN, PORT_MAX));
 			
 		strcat(iCallIt, request);
 		strcat(iCallIt, masher);
